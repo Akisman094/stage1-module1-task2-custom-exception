b2a7330c67a065f8b16035f73e36860146096309
Akmal Ismoilov <master80098@gmail.com>
2022-08-20 10:20:38 +0500

    Task completed!

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,16 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException
+  {
+    try
+    {
+      return Student.getValueOf(studentID);
+    }
+    catch (IllegalArgumentException e)
+    {
+      throw new StudentNotFoundException(e.getMessage(), e);
+    }
   }
 
   public static void main(String[] args) {
